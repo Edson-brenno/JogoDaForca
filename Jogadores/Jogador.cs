640940ad702e1c1933d8bd8b42932d91267c75a9
brenno <brennorebeiro@gmail.com>
2023-02-05 22:06:57 -0300

    Adiciona a classe jogador

---
+++ Jogadores/Jogador.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace jogoDaForca.Jogadores{
+
+    public class Jogador{
+
+        // Atributes
+        // Atributos
+        public string Nickname {get; private set;} // Will achieve the player Nickname
+
+        // Method
+        // Metodos:
+        
+        public Jogador(string nickname){
+
+            this.Nickname = nickname;
+        }
+        
+    }
+}
